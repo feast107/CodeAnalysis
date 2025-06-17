f3c1935fed4ebdd7b932156c8f4c1da70673aaa8
Feast <feast107@gmail.com>
2025-06-17 10:21:31 +0800

    Update versions

--- src/Feast.CodeAnalysis.TestGenerator/SampleAttribute.cs
+++ src/Feast.CodeAnalysis.TestGenerator/SampleAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 namespace Feast.CodeAnalysis.TestGenerator;
+
 [Literal("System.SampleAttribute")]
 [AttributeUsage(AttributeTargets.Class)]
 public class SampleAttribute(params Type[] types) : Attribute
